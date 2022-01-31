0b6518eb1a38cece53e37d0bde8e35ad0ad953fb
sanket-thange <64524353+sanket-thange@users.noreply.github.com>
2022-01-31 19:03:16 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -13,3 +13,21 @@ int largestElement(vector<int> arr) {
      return largest;
 }
 
+
+//Maximum subarray sum
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumSubarray(vector<int> A) {
+    int maximumSum = INT_MIN;
+    int currentSum = 0;
+    for(int i = 0; i < A.size() ; i++){
+        currentSum = currentSum + A[i];
+        if(currentSum>maximumSum)maximumSum = currentSum;
+        if(currentSum<0){
+            currentSum = 0;
+        }
+    }
+    return maximumSum;
+}
+
